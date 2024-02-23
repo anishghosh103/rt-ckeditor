914154ba7b0a4c97ca207e70b6d8e406606fc458
Anish Ghosh <anishghosh103@gmail.com>
2024-02-23 09:28:37 +0530

    v0.0.1: initial commit

---
+++ src/ckeditor.ts
@@ -0,0 +1,180 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { DecoupledEditor } from '@ckeditor/ckeditor5-editor-decoupled';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import {
+  Bold,
+  Italic,
+  Strikethrough,
+  Underline,
+} from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import {
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+} from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import {
+  AutoImage,
+  Image,
+  ImageCaption,
+  ImageInsert,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+} from '@ckeditor/ckeditor5-image';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { Link, LinkImage } from '@ckeditor/ckeditor5-link';
+import { List, ListProperties, TodoList } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import {
+  Table,
+  TableCellProperties,
+  TableProperties,
+  TableToolbar,
+} from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends DecoupledEditor {
+  public static override builtinPlugins = [
+    Alignment,
+    AutoImage,
+    Autoformat,
+    BlockQuote,
+    Bold,
+    Essentials,
+    FontBackgroundColor,
+    FontColor,
+    FontFamily,
+    FontSize,
+    Heading,
+    HorizontalLine,
+    Image,
+    ImageCaption,
+    ImageInsert,
+    ImageResize,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    LinkImage,
+    List,
+    ListProperties,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
+    Strikethrough,
+    Table,
+    TableCellProperties,
+    TableProperties,
+    TableToolbar,
+    TextTransformation,
+    TodoList,
+    Underline,
+  ];
+
+  public static override defaultConfig = {
+    toolbar: {
+      items: [
+        'heading',
+        '|',
+        'fontSize',
+        'fontFamily',
+        '|',
+        'fontColor',
+        'fontBackgroundColor',
+        '|',
+        'bold',
+        'italic',
+        'underline',
+        'strikethrough',
+        '|',
+        'alignment',
+        '|',
+        'numberedList',
+        'bulletedList',
+        '|',
+        'outdent',
+        'indent',
+        '|',
+        'todoList',
+        'link',
+        'blockQuote',
+        'imageUpload',
+        'insertTable',
+        'mediaEmbed',
+        '|',
+        'undo',
+        'redo',
+      ],
+    },
+    language: 'en',
+    heading: {
+      options: [
+        {
+          model: 'paragraph' as 'paragraph',
+          title: 'Paragraph',
+          class: 'ck-heading_paragraph',
+        },
+        {
+          model: 'heading1' as 'heading1',
+          view: 'h1',
+          title: 'Heading 1',
+          class: 'ck-heading_heading1',
+        },
+        {
+          model: 'heading2' as 'heading2',
+          view: 'h2',
+          title: 'Heading 2',
+          class: 'ck-heading_heading2',
+        },
+        {
+          model: 'heading3' as 'heading3',
+          view: 'h3',
+          title: 'Heading 3',
+          class: 'ck-heading_heading3',
+        },
+      ],
+    },
+    image: {
+      resizeUnit: '%' as '%',
+      toolbar: [
+        'imageTextAlternative',
+        'toggleImageCaption',
+        'imageStyle:inline',
+        'imageStyle:block',
+        'imageStyle:side',
+        'linkImage',
+      ],
+    },
+    table: {
+      contentToolbar: [
+        'tableColumn',
+        'tableRow',
+        'mergeTableCells',
+        'tableCellProperties',
+        'tableProperties',
+      ],
+    },
+  };
+}
+
+export default Editor;
